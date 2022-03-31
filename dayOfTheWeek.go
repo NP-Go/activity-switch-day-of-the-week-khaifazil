8b5bc74c0bba9793017d09bd7faba5148e614a3d
khaifazil <khairal.fazil@gmail.com>
2022-04-01 02:12:18 +0800

    completed activity

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	//Insert code here
+	today := time.Now().Weekday()
+
+	switch today {
+	case time.Monday, time.Wednesday, time.Friday, time.Sunday:
+		fmt.Println("Today is", today, "\nIt's an odd day")
+	case time.Tuesday, time.Thursday, time.Saturday:
+		fmt.Println("Today is", today, "\nIt's an even day")
+	default:
+		fmt.Println("what day is it?!")
+	}
 }
